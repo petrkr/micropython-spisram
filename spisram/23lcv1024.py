e32bab3fc204bc884f6a66eae2a373664260333f
Petr Kracík <petrkr@petrkr.net>
2022-05-03 18:58:58 +0200

    Initial code commit

---
+++ spisram/23lcv1024.py
@@ -0,0 +1,48 @@
+# Low level driver for SPI Serial SRAM 23LCV1024
+# Copyright (c) 2022 Petr Kracik
+
+from . import SRAM
+
+class SRAM23LCV1024(SRAM):
+    INSTRUCTION_READ = 0x03
+    INSTRUCTION_WRITE = 0x02
+    INSTRUCTION_RDMR = 0x05
+    INSTRUCTION_WRMR = 0x01
+
+    def __init__(spi, cs):
+        self._spi = spi
+        self._cs = cs
+
+
+    def read(count = 1, address = 0):
+        tmp = bytearray(4)
+        tmp[0] = self.INSTRUCTION_READ
+        tmp[1] = ( address >> 16 ) & 0xFF
+        tmp[2] = ( address >> 8 ) & 0xFF
+        tmp[3] = ( address ) & 0xFF
+
+        self._cs.value(0)
+        self._spi.write(tmp)
+        data = self._spi.read(count)
+        self._cs.value(1)
+
+        return data
+
+
+    def write(data, address, check=True):
+        tmp = bytearray(4)
+        tmp[0] = self.INSTRUCTION_READ
+        tmp[1] = ( address >> 16 ) & 0xFF
+        tmp[2] = ( address >> 8 ) & 0xFF
+        tmp[3] = ( address ) & 0xFF
+
+        self._cs.value(0)
+        self.write(tmp)
+        self.write(data)
+        self._cs.value(1)
+
+        if check:
+            check = self.read(len(data), address)
+            return check == data
+        else:
+            return True
---
+++ spisram/__init__.py
@@ -0,0 +1,18 @@
+# Driver for SPI Serial SRAM
+# Copyright (c) 2022 Petr Kracik
+
+__version__ = "0.0.1"
+
+
+class SRAM:
+    def __init__(self):
+        pass
+
+
+    def read(count = 1, address = 0):
+        raise NotImplementedError()
+
+
+    def write(data, address):
+        raise NotImplementedError()
+
